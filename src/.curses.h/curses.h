753b3b7a52d73014af00b7d6a7799cdd7a471bfb
AndersonTorres <torres.anderson.85@protonmail.com>
2022-05-09 00:18:51 -0300

    teapot version 1.9

--- src/.curses.h/curses.h
+++
@@ -1,6 +0,0 @@
-#ifndef I_HATE_THIS_CURSES_H
-#define I_HATE_THIS_CURSES_H
-
-#include <ncurses.h>
-
-#endif
--- src/wgetc.h
+++ src/wgetc.h
@@ -2,14 +2,8 @@
 #define WGETC_H
 
 #include <curses.h>
+#include "main.h"
 
-#define KEY_FPAGE    (KEY_MAX+1)
-#define KEY_BPAGE    (KEY_MAX+2)
-#define KEY_QUIT     (KEY_MAX+3)
-#define KEY_MENTER   (KEY_MAX+4)
-#define KEY_LOAD     (KEY_MAX+5)
-#define KEY_SAVEQUIT (KEY_MAX+6)
-
-chtype wgetc(void);
+Key wgetc(void);
 
 #endif
